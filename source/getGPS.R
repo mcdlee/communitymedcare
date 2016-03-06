7b2c2a34029cae17314a724322bace34f96501c5
mcdlee <mcdlee@gmail.com>
2016-03-06 12:31:08 +0800

    改變 parser

--- source/getGPS.R
+++ source/getGPS.R
@@ -7,25 +7,30 @@ clinic.list$hos1Ref <- as.factor(hos1Ref)
 clinic.list$hos2Ref <- as.factor(hos2Ref)
 clinic.list$hos3Ref <- as.factor(hos3Ref)
 clinic.list$hos4Ref <- as.factor(hos4Ref)
-detach(clinic.list)
 
 library(ggmap)
+library(leafletR)
+library(scales)
 
 # geocode 很脆弱
-sub.1 <- head(clinic.list, 1200)
-sub.2 <- tail(head(clinic.list, 2400), 1200)
-sub.3 <- tail(clinic.list, 635)
+sub.1 <- head(clinic.list, 900)
+sub.2 <- tail(head(clinic.list, 1800), 900)
+sub.3 <- tail(head(clinic.list, 2700), 900)
+sub.4 <- tail(clinic.list, 301)
 
-gps.1 <- geocode(as.character(sub.1$clinicAddr))
+gps.1 <- geocode(iconv(as.character(sub.1$clinicAddr), to="utf8"), source="google")
 sub.1 <- cbind(sub.1, gps.1)
 
-gps.2 <- geocode(as.character(sub.2$clinicAddr))
+gps.2 <- geocode(iconv(as.character(sub.2$clinicAddr), to="utf8"), source="google")
 sub.2 <- cbind(sub.2, gps.2)
 
-gps.3 <- geocode(as.character(sub.3$clinicAddr))
+gps.3 <- geocode(iconv(as.character(sub.3$clinicAddr), to="utf8"), source="google")
 sub.3 <- cbind(sub.3, gps.3)
 
-clinic.list.gps<-rbind(sub.1, sub.2, sub.3)
+gps.4 <- geocode(iconv(as.character(sub.4$clinicAddr), to="utf8"), source="google")
+sub.4 <- cbind(sub.4, gps.4)
+
+clinic.list.gps<-rbind(sub.1, sub.2, sub.3, sub.4)
 
 write.csv(clinic.list.gps, "../content/clinicGPS.csv")
 # backup
--- source/parsing.py
+++ source/parsing.py
@@ -13,45 +13,49 @@ def rm_blank(s):
     u = s.replace(" ", "").replace("\t", "").replace("\n", "").replace("\r", "")
     return u
 
-db = [["clinicRef", "clinicName", "clinicAddr", "city", "groupRef", "groupName", "hos1Ref", "hos1Name", "hos2Ref", "hos2Name", "hos3Ref", "hos3Name", "hos4Ref", "hos4Name"]]
+db = [["clinicRef", "clinicName", "clinicAddr", "clinicTel", "city", "groupRef", "groupName", "groupTel", "hos1Ref", "hos1Name", "hos2Ref", "hos2Name", "hos3Ref", "hos3Name", "hos4Ref", "hos4Name"]]
 
 url_primer = 'http://www.nhi.gov.tw/OnlineQuery/FamilyDrSearch.aspx?menu=20&menu_id=926&webdata_id=3661&WD_ID=929&QueryType=2&City=&Area=&HName=&HID=&CName=&CID=&H1Name=&H1ID=&page='
-for t in range(1,305):
+for t in range(0, 302):
     url = url_primer +`t`
     html = urllib2.urlopen(url).read()
     tree = etree.HTML(html)
-    clin_list = tree[1][0][2][1][3][6][0][0][1][1][0][0][0]
+    #clin_list = tree[1][0][2][1][3][5][0][0][1][1][0][0][0]
+    clin_list = tree[1][0][1][0][4][1][6][0][1][1][0][0][0]
     for i in range(1,len(clin_list)):
-        clinic_ref = rm_blank(clin_list[i][3].text.encode('utf-8'))
-        clinic_name = rm_blank(clin_list[i][3][1].text.encode('utf-8'))
-        clinic_addr = rm_blank(unicodedata.normalize('NFKC', clin_list[i][3][3].tail).encode('utf-8'))
-        city = rm_blank(clin_list[i][0].text.encode('utf-8'))
-        group_ref = rm_blank(clin_list[i][1][0].text.encode('utf-8'))
-        group_name = rm_blank(clin_list[i][1][0][0].tail.encode('utf-8'))
-        hos_1_ref = rm_blank(clin_list[i][2][0].text.encode('utf-8')).replace("(第一)", "")
-        hos_1_name = rm_blank(clin_list[i][2][0][0].tail.encode('utf-8'))
+        clinic_ref = rm_blank(clin_list[i][3].text)
+        clinic_name = rm_blank(clin_list[i][3][1].text)
+        clinic_addr = rm_blank(unicodedata.normalize('NFKC', clin_list[i][3][3].tail))
+        clinic_tel = rm_blank(clin_list[i][3][2].tail)
+        group_tel = rm_blank(clin_list[i][4].text)
+        city = rm_blank(clin_list[i][0].text)
+        group_ref = rm_blank(clin_list[i][1][0].text)
+        group_name = rm_blank(clin_list[i][1][0][0].tail)
+        hos_1_ref = rm_blank(clin_list[i][2][0].text).replace("(第一)", "")
+        hos_1_name = rm_blank(clin_list[i][2][0][0].tail)
         if len(clin_list[i][2][1]) > 0:
-            hos_2_ref = rm_blank(clin_list[i][2][1].text.encode('utf-8')).replace("(第二)", "")
-            hos_2_name = rm_blank(clin_list[i][2][1][0].tail.encode('utf-8'))
+            hos_2_ref = rm_blank(clin_list[i][2][1].text).replace("(第二)", "")
+            hos_2_name = rm_blank(clin_list[i][2][1][0].tail)
         else:
             hos_2_ref = ''
             hos_2_name = ''
         if len(clin_list[i][2][2]) > 0:
-            hos_3_ref = rm_blank(clin_list[i][2][2].text.encode('utf-8')).replace("(第三)", "")
-            hos_3_name = rm_blank(clin_list[i][2][2][0].tail.encode('utf-8'))
+            hos_3_ref = rm_blank(clin_list[i][2][2].text).replace("(第三)", "")
+            hos_3_name = rm_blank(clin_list[i][2][2][0].tail)
         else:
             hos_3_ref = ''
             hos_3_name = ''
         if len(clin_list[i][2][3]) > 0:
-            hos_4_ref = rm_blank(clin_list[i][2][3].text.encode('utf-8')).replace("(第四)", "")
-            hos_4_name = rm_blank(clin_list[i][2][3][0].tail.encode('utf-8'))
+            hos_4_ref = rm_blank(clin_list[i][2][3].text).replace("(第四)", "")
+            hos_4_name = rm_blank(clin_list[i][2][3][0].tail)
         else:
             hos_4_ref = ''
             hos_4_name = ''
-        db.append([clinic_ref, clinic_name, clinic_addr, city, group_ref, group_name, hos_1_ref, hos_1_name, hos_2_ref, hos_2_name, hos_3_ref, hos_3_name, hos_4_ref, hos_4_name])
-	print("Add a new record")
+        db.append([clinic_ref, clinic_name, clinic_addr, clinic_tel, city, group_ref, group_name, group_tel, hos_1_ref, hos_1_name, hos_2_ref, hos_2_name, hos_3_ref, hos_3_name, hos_4_ref, hos_4_name])
+        print("add 1 data")
+
 import csv
 
-with open("../content/clinic_list.csv", "wb") as f:
+with open("../content/tmp.csv", "wb") as f:
     writer = csv.writer(f)
     writer.writerows(db)
